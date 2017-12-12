383623cce34e53f20634c20b186d6cbed5c6c41c
nzhul <dobromirivanov1@gmail.com>
2017-12-12 23:11:39 +0200

    up

---
+++ pathfinding-in-unity/Assets/Scripts/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class DemoController : MonoBehaviour
+    {
+        public MapData mapData;
+        public Graph graph;
+
+        private void Start()
+        {
+            if (mapData != null && graph != null)
+            {
+                int[,] mapInstance = mapData.MakeMap();
+                graph.Init(mapInstance);
+
+                GraphView graphView = graph.gameObject.GetComponent<GraphView>();
+
+                if (graphView != null)
+                {
+                    graphView.Init(graph);
+                }
+            }
+        }
+    }
+}
---
+++ pathfinding-in-unity/Assets/Scripts/GraphView.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Graph))]
+    public class GraphView : MonoBehaviour
+    {
+        public GameObject nodeViewPrefab;
+
+        public Color baseColor = Color.white;
+        public Color wallColor = Color.black;
+
+        public void Init(Graph graph)
+        {
+            if (graph == null)
+            {
+                Debug.LogWarning("Graphiview no graph to initialize!");
+                return;
+            }
+
+            foreach (Node n in graph.nodes)
+            {
+                GameObject instance = Instantiate(nodeViewPrefab, Vector3.zero, Quaternion.identity);
+                NodeView nodeView = instance.GetComponent<NodeView>();
+
+                if (nodeView != null)
+                {
+                    nodeView.Init(n);
+
+                    if (n.nodeType == NodeType.Blocked)
+                    {
+                        nodeView.ColorNode(wallColor);
+                    }
+                    else
+                    {
+                        nodeView.ColorNode(baseColor);
+                    }
+                }
+            }
+
+        }
+    }
+}
